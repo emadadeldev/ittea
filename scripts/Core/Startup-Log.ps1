55e27854c38faeab047e2778233bc05c0c3887e9
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2025-10-29 05:12:17 +0300

    Update Startup-Log.ps1

--- scripts/Core/Startup-Log.ps1
+++ scripts/Core/Startup-Log.ps1
@@ -19,25 +19,30 @@ function Startup {
             }
             catch {
                 Add-Log -Message "Unstable internet connection detected." -Level "info"
-                Start-Sleep 8
+                Start-Sleep 10
                 UsageCount
             }
         }
         function Quotes {
-            $q=(Invoke-RestMethod "https://raw.githubusercontent.com/emadadeldev/ittea/refs/heads/main/static/Database/Quotes.json").Quotes|Sort-Object {Get-Random}
+            $q = (Invoke-RestMethod "https://raw.githubusercontent.com/emadadeldev/ittea/refs/heads/main/static/Database/Quotes.json").Quotes | Sort-Object { Get-Random }
             Set-Statusbar -Text "☕ $($itt.database.locales.Controls.$($itt.Language).welcome)"; Start-Sleep 18
             Set-Statusbar -Text "👁‍🗨 $($itt.database.locales.Controls.$($itt.Language).easter_egg)"; Start-Sleep 18
-            $i=@{quote="💬";info="📢";music="🎵";Cautton="⚠";default="☕"}
-            while(1){foreach($x in $q){$c=$i[$x.type];if(-not $c){$c=$i.default};$t="`“$($x.text)`”";if($x.name){$t+=" ― $($x.name)"};Set-Statusbar -Text "$c $t";Start-Sleep 25}}
+            $i = @{quote = "💬"; info = "📢"; music = "🎵"; Cautton = "⚠"; default = "☕" }
+            while (1) { foreach ($x in $q) { $c = $i[$x.type]; if (-not $c) { $c = $i.default }; $t = "`“$($x.text)`”"; if ($x.name) { $t += " ― $($x.name)" }; Set-Statusbar -Text "$c $t"; Start-Sleep 25 } }
         }
         function LOG {
             Write-Host "  `n` "
-            Write-Host "  ███████████████████╗ My old GitHub account was restricted without any reason from GitHub support"
-            Write-Host "  ██╚══██╔══╚═══██╔══╝ This is the new repository, please share it so people can easily"
-            Write-Host "  ██║  ██║ Emad ██║    offical repo: https://github.com/emadadeldev/ittea"
-            Write-Host "  ██║  ██║ Adel ██║    Backup:https: //gitlab.com/emadadel/itt/"
-            Write-Host "  ██║  ██║      ██║    "
-            Write-Host "  ╚═╝  ╚═╝      ╚═╝    "
+            Write-Host "  ███████████████████╗ " -NoNewline
+            Write-Host "My old GitHub account was restricted without any reason." -ForegroundColor Yellow
+            Write-Host "  ██╚══██╔══╚═══██╔══╝ " -NoNewline
+            Write-Host "This is the new official repo:" -ForegroundColor Yellow
+            Write-Host "  ██║  ██║ Emad ██║    " -NoNewline
+            Write-Host "Main repository: https://github.com/emadadeldev/ittea" -ForegroundColor Yellow
+            Write-Host "  ██║  ██║ Adel ██║    " -NoNewline
+            Write-Host "Backup 1: https://gitlab.com/emadadel/itt" -ForegroundColor Yellow
+            Write-Host "  ██║  ██║      ██║    " -NoNewline
+            Write-Host "Backup 2: https://codeberg.org/emadadel/itt" -ForegroundColor Yellow
+            Write-Host "  ╚═╝  ╚═╝      ╚═╝    " -ForegroundColor White
             UsageCount
         }
         # debug start
