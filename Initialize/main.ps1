f6e7e1c9928692a597a3de4a1ec478b511759201
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2025-12-07 06:39:57 +0200

    Update main.ps1

--- Initialize/main.ps1
+++ Initialize/main.ps1
@@ -68,6 +68,7 @@ $itt["window"].add_Closing($onClosingEvent)
 $itt["window"].Add_PreViewKeyDown($KeyEvents)
 
 # Show Window
+$itt["window"].Activate() | Out-Null
 $itt["window"].ShowDialog() | Out-Null
 
 # Dispose of runspaces and other objects
