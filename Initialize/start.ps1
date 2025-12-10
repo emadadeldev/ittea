7d645633eb4f812e2fb2325c4fe99b1ef3263c40
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2025-12-10 06:30:46 +0200

    Maxmize console window

--- Initialize/start.ps1
+++ Initialize/start.ps1
@@ -5,7 +5,6 @@ param (
     # Quick install
     [string]$i
 )
-
 # Load DLLs
 Add-Type -AssemblyName 'System.Windows.Forms', 'PresentationFramework', 'PresentationCore', 'WindowsBase','System.Net.Http'
 # ================================
@@ -57,7 +56,10 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 # ================================
 #endregion Ask user for administrator privileges if not already running as admin
 # ================================
-
+$Host.UI.RawUI.WindowTitle = "Install Twaeks Tool"
+Write-Host "`n  Relax, good things are loading… almost there!" -ForegroundColor Yellow
+$Host.UI.RawUI.BackgroundColor = 'Black'
+Clear-Host
 # ================================
 #region MAXIMIZE CURRENT WINDOW
 # ================================
@@ -80,11 +82,5 @@ $hwnd = [Win32]::GetConsoleWindow()
 
 # Create directory if it doesn't exist
 if (-not (Test-Path -Path $itt.ittDir)) {New-Item -ItemType Directory -Path $itt.ittDir -Force | Out-Null}
-
-$Host.UI.RawUI.WindowTitle = "Install Twaeks Tool"
-$Host.UI.RawUI.BackgroundColor = 'Black'
-Clear-Host
-Write-Host "`n  Relax, good things are loading… almost there!" -ForegroundColor Yellow
-
 # Trace the script
 Start-Transcript -Path (Join-Path $itt.ittDir "logs\log_$(Get-Date -Format 'yyyy-MM-dd').log") -Append -Force *> $null
