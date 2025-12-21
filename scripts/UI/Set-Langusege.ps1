68aeb56c09fcf6e4fc5cd89e96fa365528c281b5
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2025-12-21 06:39:14 +0200

    Update Set-Langusege.ps1

--- scripts/UI/Set-Langusege.ps1
+++ scripts/UI/Set-Langusege.ps1
@@ -2,11 +2,11 @@ function System-Default {
 
     try {
 
-        $dc = $itt.database.locales.Controls.$shortCulture
+        $dc = $localsUrl.Controls.$shortCulture
 
         if (-not $dc -or [string]::IsNullOrWhiteSpace($dc)) {
             Set-Statusbar -Text "Your default system language is not supported yet, fallback to English"
-            $dc = $itt.database.locales.Controls.en
+            $dc = $localsUrl.Controls.en
         }
         
         $itt["window"].DataContext = $dc
@@ -23,7 +23,7 @@ function Set-Language {
     if ($lang -eq "default") { System-Default }
     else {
         $itt.Language = $lang
-        $itt["window"].DataContext = $itt.database.locales.Controls.$($itt.Language)
+        $itt["window"].DataContext = $localsUrl.Controls.$($itt.Language)
         Set-ItemProperty -Path $itt.registryPath -Name "locales" -Value $lang -Force
     }
 }
