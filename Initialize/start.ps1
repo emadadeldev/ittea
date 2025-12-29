07e5bc83c21ec3e4764dfaeb74a266c52eb7d8b3
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2025-12-29 10:22:55 +0200

    Update start.ps1

--- Initialize/start.ps1
+++ Initialize/start.ps1
@@ -1,6 +1,27 @@
+# ================================
+#region MAXIMIZE CURRENT WINDOW
+# ================================
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public class Win32 {
+    [DllImport("user32.dll")]
+    public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetConsoleWindow();
+}
+"@
+
+$hwnd = [Win32]::GetConsoleWindow()
+[Win32]::ShowWindowAsync($hwnd, 3) | Out-Null
+# ================================
+#endregion MAXIMIZE CURRENT WINDOW
+# ================================
+
 # Console Color
 $Host.UI.RawUI.BackgroundColor = 'Black'
 $Host.UI.RawUI.WindowTitle = "Install Twaeks Tool"
+
 # Clear Consle
 Clear-Host
 
@@ -65,26 +86,6 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 }
 # ================================
 #endregion Ask user for administrator privileges if not already running as admin
-# ================================
-
-# ================================
-#region MAXIMIZE CURRENT WINDOW
-# ================================
-Add-Type @"
-using System;
-using System.Runtime.InteropServices;
-public class Win32 {
-    [DllImport("user32.dll")]
-    public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
-    [DllImport("kernel32.dll")]
-    public static extern IntPtr GetConsoleWindow();
-}
-"@
-
-$hwnd = [Win32]::GetConsoleWindow()
-[Win32]::ShowWindowAsync($hwnd, 3) | Out-Null
-# ================================
-#endregion MAXIMIZE CURRENT WINDOW
 # ================================
 
 # Create directory if it doesn't exist
