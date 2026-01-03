8e5843bbbe3c552ac4c0ef86ca3a10aba2f66e1d
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2026-01-03 04:05:08 +0200

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -62,8 +62,6 @@ Read-Host "   Press Enter to visit https://github.com/emadadeldev/ittea"
 Start-Process "https://github.com/emadadeldev/ittea"
 exit
 }
-Write-Host "  Status           [$($itt.api.message)]" -ForegroundColor Green
-Write-Host "  Version          [$($itt.Version)]" -ForegroundColor Green
 Write-Host "`n  Relax, good things are loading… almost there!`n"
 break
 }
@@ -870,11 +868,11 @@ while (1) { foreach ($x in $q) { $c = $i[$x.type]; if (-not $c) { $c = $i.defaul
 }
 function LOG {
 Write-Host "  ███████████████████╗ " -NoNewline
-Write-Host "My old GitHub account was restricted without any reason." -ForegroundColor Gray
+Write-Host "Status  [$($itt.api.message)]" -ForegroundColor Green
 Write-Host "  ██╚══██╔══╚═══██╔══╝ " -NoNewline
-Write-Host "This is the new official repo:" -ForegroundColor Gray
+Write-Host "Version [$($itt.Version)]" -ForegroundColor Green
 Write-Host "  ██║  ██║ Emad ██║    " -NoNewline
-Write-Host "Main repository: https://github.com/emadadeldev/ittea" -ForegroundColor Gray
+Write-Host "Main repository: https://github.com/emadadeldev/ittea" -ForegroundColor Yellow
 Write-Host "  ██║  ██║ Adel ██║    " -NoNewline
 Write-Host "Backup 1: https://gitlab.com/emadadel/itt" -ForegroundColor Gray
 Write-Host "  ██║  ██║      ██║    " -NoNewline
