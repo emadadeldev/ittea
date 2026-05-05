ffee697aaeddadc2a67d43cd30843a718543f7a8
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2026-05-05 05:26:36 +0300

    Update About-Dev.ps1

--- scripts/UI/About-Dev.ps1
+++ scripts/UI/About-Dev.ps1
@@ -1,5 +1,5 @@
 function About {
     $aboutPopup = $itt['window'].FindName('AboutPopup')
-    $aboutPopup.FindName('ver').Text = "Version $($itt.version) $($itt.api.message)"
+    $aboutPopup.FindName('ver').Text = "$($itt.version) $($itt.api.message)"
     $aboutPopup.IsOpen = $true
 }
