abf6561d71b04aceffd5764da648f24771f0ef0c
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2026-07-16 00:54:50 +0300

    Update start.ps1

--- Initialize/start.ps1
+++ Initialize/start.ps1
@@ -50,8 +50,8 @@ if(-not $Debug)
 
             if ($latestVersion -ne $itt.version) {
                 $splash.Close()
-                Write-Host "YOU ARE USING AN OLD VERSION OF ITT. PORTABLE SCRIPT IS NOT RECOMMENDED`nPLEASE USE THE LATEST VERSION FROM THE OFFICIAL COMMANDS AT https://github.com/emadadeldev/ittea" -ForegroundColor Red
-                Read-Host "Press Enter to visit https://github.com/emadadeldev/ittea"
+                Write-Host "  YOU ARE USING AN OLD VERSION OF ITT. PORTABLE SCRIPT IS NOT RECOMMENDED`nPLEASE USE THE LATEST VERSION FROM THE OFFICIAL COMMANDS AT https://github.com/emadadeldev/ittea" -ForegroundColor Red
+                Read-Host "  Press Enter to visit https://github.com/emadadeldev/ittea"
                 Start-Process "https://github.com/emadadeldev/ittea"
                 exit
             }
