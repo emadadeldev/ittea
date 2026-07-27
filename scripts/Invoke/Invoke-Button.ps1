f113d12aa0867b8ba92630c73e895fb56cbe4ea3
Emad Adel <236038631+emadadeldev@users.noreply.github.com>
2026-07-27 15:53:47 +0300

    Fix IDM

--- scripts/Invoke/Invoke-Button.ps1
+++ scripts/Invoke/Invoke-Button.ps1
@@ -119,7 +119,7 @@ function Invoke-Button {
         }
         "idm" {
             Add-Log -Message "Running IDM Activation..." -Level "info"
-            ITT-ScriptBlock -ScriptBlock { curl.exe -L -o $env:TEMP\\IDM_Trial_Reset.exe "https://github.com/itt-co/itt-packages/raw/refs/heads/main/automation/idm-trial-reset/IDM%20Trial%20Reset.exe"; cmd /c "$env:TEMP\\IDM_Trial_Reset.exe" }
+            Start-Process "https://github.com/J2TEAM/idm-trial-reset" 
         }
         "winoffice" {
             Start-Process "https://linkjust.com/massgrave" 
